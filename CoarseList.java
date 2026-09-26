8b36d4710c4917ac0e392ffad7b9919e181c89d2
Logan <lvdm23@outlook.com>
2026-09-26 13:46:51 +0200

    Added CoarseList implementation and given files

---
+++ CoarseList.java
@@ -0,0 +1,98 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CoarseList 
+{
+
+    private final Node head;
+    private final Node tail;
+
+    private final Lock lock = new ReentrantLock();
+
+    public CoarseList() 
+    {
+        head = new Node(Integer.MIN_VALUE);
+        tail = new Node(Integer.MAX_VALUE);
+
+        head.next = tail;
+    }
+
+    public boolean add(int value) 
+    {
+        lock.lock();
+        try
+        {
+            Node pred = head;
+            Node curr = pred.next;
+
+            while (curr.value < value)
+            {
+                pred = curr;
+                curr=curr.next;
+            }
+
+            if(curr.value == value)
+            {
+                return false;
+            }
+            else
+            {
+                Node node = new Node(value);
+                node.next = curr;
+                pred.next = node;
+                return true;
+
+            }
+        }
+        finally
+        {
+            lock.unlock();
+        }
+    }
+
+    public boolean remove(int value) 
+    {
+        lock.lock();
+        try
+        {
+            Node pred= head;
+            Node curr= pred.next;
+            while( curr.value < value)
+            {
+                pred = curr;
+                curr = curr.next;
+            }
+            if( curr.value == value)
+            {
+                pred.next = curr.next;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        finally
+        {
+            lock.unlock();
+        }
+    }
+
+    public boolean contains(int value) 
+    {
+      lock.lock();
+      try
+      {
+        Node curr = head;
+        while (curr.value < value)
+        {
+            curr = curr.next;
+        }
+        return curr.value == value;
+      }
+      finally
+      {
+        lock.unlock();
+      }
+    }
+}
